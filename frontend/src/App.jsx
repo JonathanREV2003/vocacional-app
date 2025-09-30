af4fcbb3e6d3a7aef9c628837915ad4480cfc00c
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2025-09-29 20:49:20 -0600

    primer commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <img className="absolute top-0 right-0 opacity-60 -z-1" src= "/gradient.png" alt="Gradient-img" />
+      
+      <div>
+        
+      </div>
+    
+    </main>
+  )
+}
