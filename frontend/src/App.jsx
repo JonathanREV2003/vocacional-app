39bc92117ccda85bd69a3c8839976b28071678f6
alejandrov_0 <josevillafuer3@gmail.com>
2025-10-20 20:50:48 -0600

    DashboardAdd new Dashboard page and update styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/main';
+import Dashboard from './pages/Dashboard';
 import './App.css'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </main>
   </Router>
